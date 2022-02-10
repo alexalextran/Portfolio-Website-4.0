166fa5487846bbab2edd591a842b079ab925c0c8
alexalextran <alextran8385@gmail.com>
2022-02-10 23:36:36 +1100

    landing page template

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,9 +6,10 @@ const Landing = () => {
         <section className='landing'>
             <Nav />
             <div className='landing__text'>
-            <h3>hi im </h3>
+            <h3 className='landing__text--hi'>Hi Im </h3>
                 <h1 className='landing__text--name'>Alex Tran</h1>
-                <p>Future Front End Developer</p>
+                <p className='landing__text--title'>Future Front End Developer</p>
+                <p>Im aspiring to become a react developer blah blah blah more text heeheheheheh</p>
             </div>
         </section>
     );
