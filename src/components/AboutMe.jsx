43bbce5d3e9f55954cdaacc2e107b494d6359c0f
alexalextran <alextran8385@gmail.com>
2022-02-11 19:55:44 +1100

    about me page structure completed

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import profilepic from "../assets/PROFILEPIC.jpg";
 
 const Aboutme = () => {
-    return (
+  return (
+    <section className="aboutme">
+      <div className="aboutme__profileimage">
+        <img className="aboutme__profileimage--jpg" src={profilepic}></img>
+      </div>
+      <div className="aboutme__info">
+      <div className="aboutme__info--wrapper">
+
+      
         <div>
-            about me
+          <h1>About Me</h1>
+          <p>
+            Im an student programmer that is currently learning about front end
+            development, i aspire to one day become a seinor react developer or
+            a full stack developer
+          </p>
         </div>
-    );
-}
+        <div>
+          <ul className="aboutme__info--details">
+            <li className="aboutme__info--li">Name: Alex Tran</li>
+            <li className="aboutme__info--li">Age: 18 years</li>
+            <li className="aboutme__info--li">Country: Australia, NSW</li>
+            <li className="aboutme__info--li">University: University Of Technology</li>
+            <li className="aboutme__info--li">Avaliability: Interested in any front end position</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Aboutme;
