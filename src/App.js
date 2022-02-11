2424b6dd26adf1e0724536457fac7ba6637cc4cc
alexalextran <alextran8385@gmail.com>
2022-02-11 13:45:28 +1100

    about me section boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
+import Aboutme from './components/AboutMe';
 
 
 function App() {
   return (
+    <>
    <Landing />
+   <Aboutme />
+   </>
   );
 }
 
