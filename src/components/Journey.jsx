e5bd3d5d69e9177c8d3ae221b329397baf97a0e4
alexalextran <alextran8385@gmail.com>
2022-02-12 00:07:04 +1100

    timeline foundation is down

---
+++ src/components/Journey.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Journey = () => {
+    return (
+        <section className='journey'>
+        <h1 className='journey__title'>My Journey</h1>
+        <div class="timeline">
+  <div class="container left">
+    <div class="content">
+      <h2></h2>
+      <p>Lorem ipsum..</p>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content">
+      <h2>2016</h2>
+      <p>Lorem ipsum..</p>
+    </div>
+  </div>
+</div>
+</section>
+    );
+}
+
+export default Journey;
