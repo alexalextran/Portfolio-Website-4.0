c72e30cda7ce71678a794f075548ff1593d8d565
alexalextran <alextran8385@gmail.com>
2022-02-11 21:49:37 +1100

    got the foundations down for the skills page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
 import Aboutme from './components/AboutMe';
+import Mylanguages from './components/MyLanguages';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
    <Landing />
    <Aboutme />
+   <Mylanguages />
    </>
   );
 }
